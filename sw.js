21ac04376537ee81e2dbca0b0d5b184d4aceef85
ligadejuegosymatematicas <ligadejuegosymatematicas@outlook.com>
2026-04-15 17:30:31 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@ const URLS_TO_CACHE = [
   "./styles.css",
   "./app.js",
   "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
